ab315e174bcb751bcdabc3462ef2c98ab60a9910
twjpenders <twj.penders@outlook.com>
2021-02-10 17:04:58 +0100

    Add solution for problem 1
    Update main.c with solution for problem 1.

--- problem1/main.c
+++ problem1/main.c
@@ -1,4 +1,26 @@
+/* Solves the problem by summing all multiples of 3 and 5 and then subtracting
+ * all multiples of 15. The remainder is the sum of all multiples of 3 or 5 but
+ * not both. This is called the inclusion-exclusion principle.
+ */
+
+#include <stdio.h>
+
 int main(int argc, char *argv[])
 {
+	int sum = 0;
+
+	for (int i = 3; i < 1000; i += 3) {
+		sum += i;
+	}
+
+	for (int i = 5; i < 1000; i += 5) {
+		sum += i;
+	}
+
+	for (int i = 15; i < 1000; i += 15) {
+		sum -= i;
+	}
+
+	printf("%d\n", sum);
 	return 0;
 }
