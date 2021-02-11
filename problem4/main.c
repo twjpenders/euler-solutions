dc3cafa639fb422b594ae3ab912baab3f4d7c02b
twjpenders <twj.penders@outlook.com>
2021-02-11 08:43:22 +0100

    Add solving method to problem 4
    Update main.c with explanation to solve and add basic loop in main
    function.

--- problem4/main.c
+++ problem4/main.c
@@ -1,6 +1,26 @@
+/* We have to check every product from 100x100 up to 999x999 if it is a
+ * palindrome. If it is we compare it to the previous palindrome to see
+ * if it is larger. If it is then this is our new candidate answer.
+ */
+
 #include <stdio.h>
+
+int is_palindrome(int number)
+{
+	return 0;
+}
 
 int main(int argc, char *argv[])
 {
-    return 0;
+	int answer = 0;
+	for (int a = 100; a < 1000; a++) {
+		for (int b = 100; b < 1000; b++) {
+			if (is_palindrome(a*b) && a*b > answer) {
+				answer = a*b;
+			} else continue;
+		}
+	}
+
+	printf("%d\n", answer);
+    	return 0;
 }
